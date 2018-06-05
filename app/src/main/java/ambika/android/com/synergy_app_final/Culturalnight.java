43db6658d5b36faef919a472caeede2ae787db70
Ambika99 <ambika.singhal99@gmail.com>
2018-06-05 21:37:31 +0530

    splash_screen_commit

--- app/src/main/java/ambika/android/com/synergy_app_final/Culturalnight.java
+++ app/src/main/java/ambika/android/com/synergy_app_final/Culturalnight.java
@@ -1,13 +1,18 @@
 package ambika.android.com.synergy_app_final;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.thekhaeng.pushdownanim.PushDownAnim;
 import com.ugurtekbas.fadingindicatorlibrary.FadingIndicator;
 
+import io.supercharge.shimmerlayout.ShimmerLayout;
 import nl.dionsegijn.konfetti.KonfettiView;
 import nl.dionsegijn.konfetti.models.Shape;
 
@@ -32,6 +37,18 @@ public class Culturalnight extends AppCompatActivity {
 
 //Set fill color
         indicator.setFillColor(Color.LTGRAY);
+        Button button = findViewById(R.id.back);
+
+        PushDownAnim.setPushDownAnimTo( button)
+                .setOnClickListener( new View.OnClickListener(){
+                    @Override
+                    public void onClick( View view ){
+                        Intent i= new Intent();
+                        i.setClass(Culturalnight.this,MainActivity.class);
+                        startActivity(i);
+                    }
+
+                } );
 
 
     }
--- app/src/main/java/ambika/android/com/synergy_app_final/MainActivity.java
+++ app/src/main/java/ambika/android/com/synergy_app_final/MainActivity.java
@@ -1,6 +1,7 @@
 package ambika.android.com.synergy_app_final;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alexfu.countdownview.CountDownView;
@@ -18,6 +20,9 @@ import com.nightonke.boommenu.BoomButtons.HamButton;
 import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
 import com.nightonke.boommenu.BoomButtons.TextInsideCircleButton;
 import com.nightonke.boommenu.BoomMenuButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     CardView aboutus,synergy,events,gallery,faqs,contactus,culturalnight;
@@ -28,13 +33,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private PopupWindow popup;
     private CoordinatorLayout clayout;
     Intent intenttab;
+    private TextView dd,hh,mm,ss;
+    private TextView eventStart,heading;
+    private Handler handler;
+    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CountDownView countDownView = findViewById(R.id.count_down);
-        countDownView.start();
         aboutus = (CardView) findViewById(R.id.aboutus);
         synergy = (CardView) findViewById(R.id.synergy);
         events = (CardView) findViewById(R.id.events);
@@ -93,7 +100,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             });
         }
+        Intent in = getIntent();
+        dd = (TextView)findViewById(R.id.txtDay);
+        hh = (TextView)findViewById(R.id.txtHour);
+        mm = (TextView)findViewById(R.id.txtMinute);
+        ss = (TextView)findViewById(R.id.txtSecond);
+        eventStart = (TextView)findViewById(R.id.eventStart);
+        heading = (TextView)findViewById(R.id.heading);
+        countDownStart();
 
+    }
+    public void countDownStart(){
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this,1000);
+                try{
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy");
+                    Date futureDate = dateFormat.parse("21-02-2019");
+                    Date currentDate = new Date();
+                    if(!currentDate.after(futureDate)){
+                        long diff = futureDate.getTime()-currentDate.getTime();
+                        long days = diff/(24*60*60*1000);
+                        diff-=days*(24*60*60*1000);
+                        long hours = diff/(60*60*1000);
+                        diff-=hours*(60*60*1000);
+                        long minutes = diff/(60*1000);
+                        diff-=minutes*(60000);
+                        long seconds = diff/1000;
+                        dd.setText(""+String.format("%02d",days));
+                        hh.setText(""+String.format("%02d",hours));
+                        mm.setText(""+String.format("%02d",minutes));
+                        ss.setText(""+String.format("%02d"+seconds));
+
+                    }
+                    else{
+                        eventStart.setVisibility(View.VISIBLE);
+                        heading.setText("HURRAH!!!");
+                        eventStart.setText("SYNERGY HAS STARTED");
+                        textViewGone();
+                    }
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+
+
+            }
+        };
+        handler.postDelayed(runnable,1*1000);
+
+    }
+    public void textViewGone(){
+        findViewById(R.id.linearLayout1).setVisibility(View.GONE);
+        findViewById(R.id.linearLayout2).setVisibility(View.GONE);
+        findViewById(R.id.linearLayout3).setVisibility(View.GONE);
+        findViewById(R.id.linearLayout4).setVisibility(View.GONE);
     }
 
     @Override
@@ -110,7 +173,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.synergy:
-                Toast.makeText(MainActivity.this,"Synergy Page",Toast.LENGTH_LONG).show();
+                Intent i5 = new Intent();
+                i5.setClass(MainActivity.this,Synergy.class);
+                startActivity(i5);
                 break;
             case R.id.events:
                 Toast.makeText(MainActivity.this,"Click on the Ham Button to get the Event List",Toast.LENGTH_LONG).show();
