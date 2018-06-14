6cca45e9d83887551b07241ce3fc55c5a81f170c
Ambika99 <ambika.singhal99@gmail.com>
2018-06-14 15:36:35 +0530

    finalcommit

--- app/src/main/java/ambika/android/com/synergy_app_final/Splash2.java
+++ app/src/main/java/ambika/android/com/synergy_app_final/Splash2.java
@@ -8,25 +8,37 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class Splash extends AppCompatActivity {
+public class Splash2 extends AppCompatActivity {
+    private ImageView vit;
+    private ImageView iete;
     private TextView tv;
     private ImageView synergy;
-    private ImageView iete;
+    private ImageView delayImage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash2);
+        vit = (ImageView) findViewById(R.id.vit);
         iete = (ImageView) findViewById(R.id.iete);
-        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
-        iete.startAnimation(myanim);
+        tv = (TextView) findViewById(R.id.tv);
+        synergy = (ImageView) findViewById(R.id.synergy);
+        delayImage = (ImageView) findViewById(R.id.delayImage);
+        Animation anim1 = AnimationUtils.loadAnimation(this,R.anim.anim1);
+        Animation anim2 = AnimationUtils.loadAnimation(this,R.anim.anim2);
+        Animation delayAnim = AnimationUtils.loadAnimation(this,R.anim.anima_delay);
+        iete.startAnimation(anim1);
+        vit.startAnimation(anim1);
+        tv.startAnimation(anim1);
+        synergy.startAnimation(anim2);
+        delayImage.startAnimation(delayAnim);
         final Intent intent = new Intent(this, MainActivity.class);
-        Thread timer = new Thread(){
+        Thread mytimer = new Thread(){
             @Override
             public void run() {
                 try{
-                    sleep(5000);
+                    sleep(3000);
                 } catch (InterruptedException e){
                     e.printStackTrace();
                 }
@@ -37,6 +49,6 @@ public class Splash extends AppCompatActivity {
                 }
             }
         };
-        timer.start();
+        mytimer.start();
     }
 }
